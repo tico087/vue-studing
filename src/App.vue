b1948af20d51c4696e07ae8a2fd4e7a15fe33d0f
ismael gonçalves <ismaelgoncalves@otimize.me>
2024-06-19 19:02:43 -0300

    feat(todo_list): implement v-for loop to render todo list with improved styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <MainHeader v-show="showHeader" />
-  <div v-show="showName">
-    <div>Nome : {{ firstName }}</div>
-    <div>Sobrenome : {{ lastName }}</div>
+  
+  <div v-show="showName" class="section">
+    <div>Nome: {{ firstName }}</div>
+    <div>Sobrenome: {{ lastName }}</div>
   </div>
 
-  <div v-show="showLevel">
-      <div v-if="accessLevel === 'admin'">Nivel Admin</div>
-      <div v-else-if="accessLevel === 'marketing'">Nivel Marketing</div>
-      <div v-else >Usuário Comum</div>
+  <div v-show="showLevel" class="section">
+    <div v-if="accessLevel === 'admin'" class="level">Nível Admin</div>
+    <div v-else-if="accessLevel === 'marketing'" class="level">Nível Marketing</div>
+    <div v-else class="level">Usuário Comum</div>
   </div>
 
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <div v-for="(todo, index) in todos" :key="todo.id" class="todo">
+    {{ index + 1 }} - {{ todo.title }}
+  </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 import MainHeader from "./components/MainHeader.vue";
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
     MainHeader,
   },
   data() {
@@ -31,20 +31,35 @@ export default {
       firstName: "Jon",
       lastName: "Snow",
       showName: true,
-      showLevel: true, 
-      accessLevel: ''
+      showLevel: true,
+      accessLevel: "admin", // Exemplo de nível de acesso
+      todos: [
+        { userId: 1, id: 1, title: "delectus aut autem", completed: false },
+        { userId: 1, id: 2, title: "quis ut nam facilis et officia qui", completed: false },
+        { userId: 1, id: 3, title: "fugiat veniam minus", completed: false },
+        { userId: 1, id: 4, title: "et porro tempora", completed: true },
+        { userId: 1, id: 5, title: "laboriosam mollitia et enim quasi adipisci quia provident illum", completed: false },
+        { userId: 1, id: 6, title: "qui ullam ratione quibusdam voluptatem quia omnis", completed: false },
+      ],
     };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.section {
+  margin-bottom: 20px;
+}
+
+.level {
+  font-weight: bold;
+  color: #0b33a1; /* Green color */
+}
+
+.todo {
+  background-color: #f2f2f2; /* Light gray background */
+  padding: 10px;
+  margin-bottom: 5px;
+  border-left: 6px solid #0b33a1; /* Green border left */
 }
 </style>
